addfc5b9ccafc3fb79925c778860b859d264e6e0
bartbie <bartbie37@gmail.com>
2023-08-01 08:49:50 +0200

    build: fixed workspace problems

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,13 +3,13 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  optimizeDeps: {
-    include: ["@pokedemo/common"],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/common/, /node_modules/],
+    plugins: [svelte()],
+    optimizeDeps: {
+        include: ["@pokedemo/common"],
     },
-  },
+    build: {
+        commonjsOptions: {
+            include: [/common/, /node_modules/],
+        },
+    },
 });
